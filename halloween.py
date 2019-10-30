3b0531c60b3295d579fab6bde16befeabfbbf6ff
Janis Lesinskis <janis@pythoncharmers.com>
2019-10-30 15:12:59 -0500

    Docstring

--- halloween.py
+++ halloween.py
@@ -8,9 +8,10 @@ import location
 class HalloweenException(Exception):
     pass
 
-def halloween_ize(func):
+def halloween_ize(spooky):
+    """Make it more Halloween!"""
     australia_called = 0
-    @wraps(func)
+    @wraps(spooky)
     def more_halloween(*args, **kwargs):
         """🎃🎃🎃 Let's make this more awesome!!! 🎃🎃🎃"""
         print(func.__name__ + " was called")
@@ -20,15 +21,16 @@ def halloween_ize(func):
             nonlocal australia_called
             australia_called += 1
             if australia_called == 1:
-                raise HalloweenException("🎃🎃🎃 Pumpkins aren't in season in australia now... 🎃🎃🎃")
+                raise HalloweenException("🎃🎃🎃 Pumpkins aren't in season in Australia now... 🎃🎃🎃")
             elif australia_called == 2:
-                raise HalloweenException("Seems like you still really want to celebrate halloween in Australia, "
+                raise HalloweenException("Seems like you still really want to celebrate Halloween in Australia, "
                                          "perhaps you should consider travelling to the northern hemisphere using "
                                          "southern_hemisphere.travel_to('Chicago') ?")
             elif australia_called >= 3:
+                
                 halloween_prefix = "🦘🦘🦘"
                 halloween_suffix = "🦘🦘🦘"
-        res = func(*args, **kwargs)
+        res = spooky(*args, **kwargs)
         if res == TRICK:
             return halloween_prefix + "🕸️ 🦇 👻 " + res + "🕸️ 🦇 👻 " + halloween_suffix
         elif res == TREAT:
